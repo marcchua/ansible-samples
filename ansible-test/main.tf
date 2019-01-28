183767a897d48d7d6ed068026e0fa903c7cc5821
Marc <marc@hashicorp.com>
2019-01-28 10:01:54 -0500

    Ansible test 5

--- ansible-test/main.tf
+++ ansible-test/main.tf
@@ -11,15 +11,16 @@ resource "null_resource" "run-ansible-playbook" {
     type = "ssh"
     host = "${var.ansible_host_addr}"
     user = "${var.user}"
-    password = "Hashi123!"
-#    private_key = "${var.ssh_private_key}"
+#    password = "Hashi123!"
+    private_key = "${var.ssh_private_key}"
   }
-
+/*
   provisioner "remote-exec" {
     inline = [
      "ansible-playbook ~/ansible-samples/${var.app}.yml -e 'ansible_python_interpreter=/usr/bin/python3' -i ${var.ansible_host_addr}  ,",
     ]
   }
+*/
 
 #  depends_on = ["vsphere_virtual_machine.vm"]
 
